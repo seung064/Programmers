3b84505563b986bbdfa9cd0338ed91b8c9bbbdd1
seung064 <gli056@naver.com>
2025-06-17 17:00:51 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.24 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        //int answer = 0;
+        //return answer;
+        if(Math.Sqrt(n) %1 ==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
+}
