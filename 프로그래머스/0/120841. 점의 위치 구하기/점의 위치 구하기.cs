053fa0e14e06f36e7a9eb6594195c610f1c61be6
seung064 <gli056@naver.com>
2025-06-17 23:10:20 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.27 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Solution {
+    public int solution(int[] dot) {
+        
+        int a = dot[0];
+        int b = dot[1];
+        
+        
+        if(a>0 && b>0)
+        {
+            return 1;
+        }
+        else if(a<0 && b>0)
+        {
+            return 2;
+        }
+        else if(a<0 && b<0)
+        {
+            return 3;
+        }
+        else if(a>0 && b<0)
+        {
+            return 4;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
