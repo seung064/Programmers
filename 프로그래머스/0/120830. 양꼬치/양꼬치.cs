3edf2f3d5d2481ff686e0b836ad028824c98bab7
seung064 <gli056@naver.com>
2025-06-17 17:00:12 +0900

    [level 0] Title: 양꼬치, Time: 0.15 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n, int k) 
+    {
+        int answer1 = 12000*n + 2000*k - 2000*(n/10);
+        
+        return answer1;
+    }
+}
