bd02ef8a533e8335042eae3980a25708a45b08f1
seung064 <gli056@naver.com>
2025-06-17 23:37:11 +0900

    [level 0] Title: 편지, Time: 0.16 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(string message) {
+        //int answer = 0;
+        //return answer;
+        
+        int length = message.Length;
+        return length*2;
+    }
+}
